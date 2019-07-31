8117f85068d150b770e33cb054e4782bd588e8ee
Toniel Lenk <toniel.moraes@amaggi.com.br>
2019-07-31 17:41:10 -0400

    Hello World!

---
+++ App.js
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Fragment} from 'react';
+import {
+  View,
+  Text
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Hello, world!</Text>
+    </View>
+  );
+};
+
+export default App;
