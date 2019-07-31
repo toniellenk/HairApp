a24c5f6d939f739ba0c36ebc14194488aecd3d53
Toniel Lenk <toniel.moraes@amaggi.com.br>
2019-07-31 17:55:09 -0400

    Frase adicional

--- App.js
+++ App.js
@@ -16,6 +16,7 @@ const App = () => {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Hello, world!</Text>
+      <Text>O mundo já não é mais o mesmo.</Text>
     </View>
   );
 };
